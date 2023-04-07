18b3f2407aeb0265fc5e69a6f395d7a3a15bab82
Julio Neto <nettojulio@hotmail.com>
2023-04-07 01:42:16 -0300

    initial commit

---
+++ src/main/java/br/com/julioneto/App.java
@@ -0,0 +1,38 @@
+package br.com.julioneto;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    private static Double valor = null;
+
+    public static void main(String[] args) {
+        entrada();
+        saida();
+    }
+
+    private static void entrada() {
+        Scanner teclado = new Scanner(System.in);
+        teclado.useLocale(Locale.US);
+
+        do {
+            System.out.print("Digite um número: ");
+            String valorDigitado = teclado.nextLine().replace(",", ".");
+            validacoesDeEntrada(valorDigitado);
+        } while (valor == null);
+
+        teclado.close();
+    }
+
+    private static void validacoesDeEntrada(String valorDigitado) {
+        try {
+            valor = Double.parseDouble(valorDigitado);
+        } catch (Exception e) {
+            System.out.println("Erro ao digitar número, tente novamente!");
+        }
+    }
+
+    private static void saida() {
+        System.out.printf("O número informado foi %f", valor);
+    }
+}
